ae27bc7563a04616db4ad4003c598b7d327eae9a
alexquali <Alexey.b@qualisystems.com>
2020-05-04 18:00:37 +0300

    Move simple connectivity logic from networking package

---
+++ cloudshell/shell/flows/connectivity/exceptions.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+class VLANHandlerException(Exception):
+    pass
+
+
+class RequestValidatorException(Exception):
+    pass
+
+
+class ApplyConnectivityException(Exception):
+    pass
