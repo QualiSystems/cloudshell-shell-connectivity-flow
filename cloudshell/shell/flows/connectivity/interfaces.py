91fb6a0cd9015dcbad3426752498969618100cdb
Kyrylo Maksymenko <saklar13@gmail.com>
2020-11-10 15:35:04 +0200

    remove unused

--- cloudshell/shell/flows/connectivity/interfaces.py
+++
@@ -1,9 +0,0 @@
-from abc import ABCMeta, abstractmethod
-
-ABC = ABCMeta("ABC", (object,), {"__slots__": ()})
-
-
-class ConnectivityFlowInterface(ABC):
-    @abstractmethod
-    def apply_connectivity_changes(self, request):
-        pass
