654acb93eb0d98941ee1ebd5578267b10c523461
Kyrylo Maksymenko <saklar13@gmail.com>
2020-11-10 15:35:04 +0200

    remove unused

--- tests/cloudshell/shell/flows/connectivity/models/test_driver_request.py
+++
@@ -1,9 +0,0 @@
-from unittest import TestCase
-
-from cloudshell.shell.flows.connectivity.models.driver_request import DriverRequest
-
-
-class TestDriverRequest(TestCase):
-    def test_driver_request(self):
-        inst = DriverRequest()
-        self.assertEqual([], inst.actions)
