b10fe27efad7879656393465efd67740d2bb485b
Kyrylo Maksymenko <saklar13@gmail.com>
2021-11-25 23:34:30 +0200

    fix typing

--- cloudshell/shell/flows/connectivity/models/driver_response.py
+++ cloudshell/shell/flows/connectivity/models/driver_response.py
@@ -1,4 +1,4 @@
-from __future__ import annotations
+from typing import Dict, List
 
 from pydantic import BaseModel
 
@@ -14,7 +14,7 @@ class ConnectivityActionResult(BaseModel):
     success: bool = True
 
     @staticmethod
-    def _action_dict(action: ConnectivityActionModel) -> dict[str, str]:
+    def _action_dict(action: ConnectivityActionModel) -> Dict[str, str]:
         return {
             "actionId": action.action_id,
             "type": action.type.value,
@@ -24,13 +24,13 @@ class ConnectivityActionResult(BaseModel):
     @classmethod
     def success_result(
         cls, action: ConnectivityActionModel, msg: str
-    ) -> ConnectivityActionResult:
+    ) -> "ConnectivityActionResult":
         return cls(**cls._action_dict(action), infoMessage=msg)
 
     @classmethod
     def success_result_vm(
         cls, action: ConnectivityActionModel, msg: str, mac_address: str
-    ) -> ConnectivityActionResult:
+    ) -> "ConnectivityActionResult":
         inst = cls.success_result(action, msg)
         inst.updatedInterface = mac_address
         return inst
@@ -38,17 +38,17 @@ class ConnectivityActionResult(BaseModel):
     @classmethod
     def fail_result(
         cls, action: ConnectivityActionModel, msg: str
-    ) -> ConnectivityActionResult:
+    ) -> "ConnectivityActionResult":
         return cls(**cls._action_dict(action), errorMessage=msg, success=False)
 
 
 class DriverResponse(BaseModel):
-    actionResults: list[ConnectivityActionResult]
+    actionResults: List[ConnectivityActionResult]
 
 
 class DriverResponseRoot(BaseModel):
     driverResponse: DriverResponse
 
     @classmethod
-    def prepare_response(cls, action_results: list[ConnectivityActionResult]):
+    def prepare_response(cls, action_results: List[ConnectivityActionResult]):
         return cls(driverResponse=DriverResponse(actionResults=action_results))
