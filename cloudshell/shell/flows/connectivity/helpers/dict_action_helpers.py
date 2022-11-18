f72a4ecd13c0866af02e8be9105745980f89b566
Kyrylo Maksymenko <saklar13@gmail.com>
2022-11-18 19:21:30 +0100

    use get/set value function for an attribute list

--- cloudshell/shell/flows/connectivity/helpers/dict_action_helpers.py
+++ cloudshell/shell/flows/connectivity/helpers/dict_action_helpers.py
@@ -1,7 +1,11 @@
 from __future__ import annotations
 
+from typing import Any
 
-def get_val_from_list_attrs(list_attrs: list[dict[str, str]], name: str) -> str:
+NOT_SET = object()
+
+
+def get_val_from_list_attrs(list_attrs: list[dict[str, Any]], name: str) -> Any:
     for attr_dict in list_attrs:
         if attr_dict["attributeName"] == name:
             return attr_dict["attributeValue"]
@@ -9,11 +13,12 @@ def get_val_from_list_attrs(list_attrs: list[dict[str, str]], name: str) -> str:
 
 
 def set_val_to_list_attrs(
-    list_attrs: list[dict[str, str]], name: str, value: str
+    list_attrs: list[dict[str, str]], name: str, value: Any, set_if_eq: Any = NOT_SET
 ) -> None:
     for attr_dict in list_attrs:
         if attr_dict["attributeName"] == name:
-            attr_dict["attributeValue"] = value
+            if set_if_eq is NOT_SET or attr_dict["attributeValue"] == set_if_eq:
+                attr_dict["attributeValue"] = value
             break
     else:
         raise KeyError(f"Attribute '{name}' not found in list of attributes")
