9dbd7268ec93ff3d9233a96f660367fb4417b173
Kyrylo Maksymenko <saklar13@gmail.com>
2023-03-14 11:19:03 +0100

    fix VLAN ID in virtual service for VLAN Auto

--- tests/cloudshell/shell/flows/connectivity/test_parse_request_service.py
+++ tests/cloudshell/shell/flows/connectivity/test_parse_request_service.py
@@ -3,6 +3,9 @@ from typing import List
 
 import pytest
 
+from cloudshell.shell.flows.connectivity.helpers.dict_action_helpers import (
+    set_val_to_list_attrs,
+)
 from cloudshell.shell.flows.connectivity.models.connectivity_model import (
     ConnectivityActionModel,
 )
@@ -37,3 +40,23 @@ def test_parse_connectivity_request_service(driver_request):
     assert first.connection_params.vlan_id == "10"
     assert second.connection_params.vlan_id == "11"
     assert first.action_id == second.action_id
+    assert first.connection_params.vlan_service_attrs.virtual_network is None
+
+
+def test_parse_connectivity_without_vlan_id_in_vlan_service(driver_request):
+    # in VLAN Auto mode, the VLAN ID is not provided in the VLAN Service
+    action_request = driver_request["driverRequest"]["actions"][0]
+    service_attrs = action_request["connectionParams"]["vlanServiceAttributes"]
+    set_val_to_list_attrs(service_attrs, "VLAN ID", "")
+    assert action_request["connectionParams"]["vlanId"] == "10-11"
+
+    service = ParseConnectivityRequestService(
+        is_vlan_range_supported=True, is_multi_vlan_supported=True
+    )
+    actions = service.get_actions(json.dumps(driver_request))
+
+    assert len(actions) == 1
+    action = actions[0]
+    assert action.connection_params.vlan_id == "10-11"
+    assert action.connection_params.vlan_service_attrs.virtual_network is None
+    assert action.connection_params.vlan_service_attrs.vlan_id == "10-11"
