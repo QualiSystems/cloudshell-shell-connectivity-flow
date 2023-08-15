174eb85ef0bcbba3a8507ef4e78a8ea4cf0ec157
Kyrylo Maksymenko <saklar13@gmail.com>
2023-08-15 13:41:26 +0200

    fix for failed response

--- cloudshell/shell/flows/connectivity/basic_flow.py
+++ cloudshell/shell/flows/connectivity/basic_flow.py
@@ -3,13 +3,16 @@ from __future__ import annotations
 import warnings
 from abc import ABC, abstractmethod
 from collections.abc import Collection
+from copy import deepcopy
 from typing import Any
 
 from attrs import define
 
 from cloudshell.shell.flows.connectivity.devices_flow import AbcDeviceConnectivityFlow
+from cloudshell.shell.flows.connectivity.exceptions import ApplyConnectivityException
 from cloudshell.shell.flows.connectivity.models.connectivity_model import (
     ConnectivityActionModel,
+    ConnectivityTypeEnum,
 )
 from cloudshell.shell.flows.connectivity.models.driver_response import (
     ConnectivityActionResult,
@@ -18,7 +21,7 @@ from cloudshell.shell.flows.connectivity.models.driver_response import (
 
 @define
 class AbstractConnectivityFlow(AbcDeviceConnectivityFlow, ABC):
-    def __attrs_post_init__(self):
+    def __attrs_post_init__(self) -> None:
         depr_msg = (
             "This class is deprecated. Use AbcDeviceConnectivityFlow or "
             "AbcCloudProviderConnectivityFlow"
@@ -47,15 +50,28 @@ class AbstractConnectivityFlow(AbcDeviceConnectivityFlow, ABC):
         # deprecated
         pass
 
-    def set_vlan(
-        self, action: ConnectivityActionModel, target: Any
-    ) -> ConnectivityActionResult:
-        return self._set_vlan(action)
+    def set_vlan(self, action: ConnectivityActionModel, target: Any) -> str:
+        result = self._set_vlan(action)
+        if not result.success:
+            raise ApplyConnectivityException(result.errorMessage)
+        return result.updatedInterface
 
-    def remove_vlan(
-        self, action: ConnectivityActionModel, target: Any
-    ) -> ConnectivityActionResult:
-        return self._remove_vlan(action)
+    def remove_vlan(self, action: ConnectivityActionModel, target: Any) -> str:
+        result = self._remove_vlan(action)
+        if not result.success:
+            raise ApplyConnectivityException(result.errorMessage)
+        return result.updatedInterface
 
     def validate_actions(self, actions: Collection[ConnectivityActionModel]) -> None:
         self._validate_received_actions(actions)
+
+    def clear(self, action: ConnectivityActionModel, target: Any) -> str:
+        """Executes before set VLAN actions and for rolling back failed."""
+        a = deepcopy(action)
+        a.connection_params.vlan_id = ""
+        a.connection_params.vlan_service_attrs.vlan_id = ""
+        a.type = ConnectivityTypeEnum.REMOVE_VLAN
+        result = self._remove_vlan(action)
+        if not result.success:
+            raise ApplyConnectivityException(result.errorMessage)
+        return result.updatedInterface
